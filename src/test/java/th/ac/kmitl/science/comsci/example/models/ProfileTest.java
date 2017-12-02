e03a1482148a19a9a7520a767e2def36941d54b8
Patipon Taweechat <57050269@kmitl.ac.th>
2017-12-02 15:01:47 +0700

    EditSpace

--- src/test/java/th/ac/kmitl/science/comsci/example/models/ProfileTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/ProfileTest.java
@@ -8,17 +8,17 @@ public class ProfileTest extends Profile {
     public void testProfile(){
         String id = "12345";
         String name = "Prayuth";
-		String globalId = "11111";
-		String taxId = "22222";
+        String globalId = "11111";
+        String taxId = "22222";
 		
         setId (id);
         setName (name);
-		setGlobalId (globalId);
-		setTaxId (taxId);
+        setGlobalId (globalId);
+        setTaxId (taxId);
 		
         assert (getId().equals(id));
         assert (getName().equals(name));
-		assert (getGlobalId().equals(globalId));
-		assert (getTaxId().equals(taxId));
+        assert (getGlobalId().equals(globalId));
+        assert (getTaxId().equals(taxId));
     }
 }
