3b89cd235dedb5255fed8fbfac15eaa8de15224d
Patipon Taweechat <57050269@kmitl.ac.th>
2017-12-02 14:55:04 +0700

    Add field globalId and taxId

--- src/main/java/th/ac/kmitl/science/comsci/example/models/Profile.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Profile.java
@@ -4,8 +4,22 @@ public abstract class Profile {
     
     private String id;
     private String name;
+    private String globalId;  
+    private String taxId;
     //TODO Create variable type of address class
     
+    public String getTaxId() {
+        return taxId;
+    }
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+    public String getGlobalId() {
+        return globalId;
+    }
+    public void setGlobalId(String globalId) {
+        this.globalId = globalId;
+    }
     public String getName() {
         return name;
     }
--- src/test/java/th/ac/kmitl/science/comsci/example/models/ProfileTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/ProfileTest.java
@@ -8,11 +8,17 @@ public class ProfileTest extends Profile {
     public void testProfile(){
         String id = "12345";
         String name = "Prayuth";
+		String globalId = "11111";
+		String taxId = "22222";
 		
         setId (id);
         setName (name);
+		setGlobalId (globalId);
+		setTaxId (taxId);
 		
         assert (getId().equals(id));
         assert (getName().equals(name));
+		assert (getGlobalId().equals(globalId));
+		assert (getTaxId().equals(taxId));
     }
 }
