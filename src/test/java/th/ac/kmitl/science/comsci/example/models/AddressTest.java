30131904d799ffebfd8dabb7a2a663c009258044
Nattadet <tooi_321@hotmail.com>
2017-12-03 14:48:52 +0700

    Modify : Test of Address

--- src/test/java/th/ac/kmitl/science/comsci/example/models/AddressTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/AddressTest.java
@@ -9,18 +9,54 @@ public class AddressTest {
         Address address = new Address();
         String result;
         int resultForCheckCitySubDivisionSection;
+        String []resultArray = new String[9];
         
         address.setHouseAddress("1103/52");
         result = address.getHouseAddress();
         assert (result.contains("1103/52"));
         
-        address.setCountrySubDivision("Bangkok");
-        result = address.getCountrySubDivision();
-        assert (result.contains("Bangkok"));
+        address.setVillage("Jarurat");
+        result = address.getVillage();
+        assert (result.contains("Jarurat"));
         
         address.setCitySubDivisionSection(1);
         resultForCheckCitySubDivisionSection = address.getCitySubDivisionSection();
         assert (resultForCheckCitySubDivisionSection==1);
+        
+        address.setStreet("Petchburi");
+        result = address.getStreet();
+        assert (result.contains("Petchaburi"));
+        
+        address.setAlley("Petchburi31");
+        result = address.getAlley();
+        assert (result.contains("Petchaburi31"));
+        
+        address.setCitySubDivision("Makkasan");
+        result = address.getCitySubDivision();
+        assert (result.contains("Makkasan"));
+        
+        address.setCity("Ratchatewee");
+        result = address.getCity();
+        assert (result.contains("Ratchatewee"));
+        
+        address.setCountrySubDivision("Bangkok");
+        result = address.getCountrySubDivision();
+        assert (result.contains("Bangkok"));
+        
+        address.setCountry("Thailand");
+        result = address.getCountry();
+        assert (result.contains("Thailand"));
+        
+        resultArray = address.getAddress();
+        assert (resultArray[0].contains("1103/52"));
+        assert (resultArray[1].contains("Jarurat"));
+        assert (resultArray[2].contains("1"));
+        assert (resultArray[3].contains("Petchaburi"));
+        assert (resultArray[4].contains("Petchaburi31"));
+        assert (resultArray[5].contains("Makkasan"));
+        assert (resultArray[6].contains("Ratchatewee"));
+        assert (resultArray[7].contains("Bangkok"));
+        assert (resultArray[8].contains("Thailand"));
     }
     
 }
