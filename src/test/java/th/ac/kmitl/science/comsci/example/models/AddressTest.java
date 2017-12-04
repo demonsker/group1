e5dec229510711436871395b0425382d9e75e519
Nattadet <tooi_321@hotmail.com>
2017-12-04 17:37:42 +0700

    Modify test case

--- src/test/java/th/ac/kmitl/science/comsci/example/models/AddressTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/AddressTest.java
@@ -3,49 +3,84 @@ package th.ac.kmitl.science.comsci.example.models;
 import org.junit.Test;
 
 public class AddressTest {
+    private Address address = new Address();
+    private int resultForCheckCitySubDivisionSection;
+    private String result;
+    private String []resultArray = new String[9];
     
     @Test
-    public void setterAndGetterMustBeMatch() {
-        Address address = new Address();
-        String result;
-        int resultForCheckCitySubDivisionSection;
-        String []resultArray = new String[9];
-        
-        address.setHouseAddress("1103/52");
-        result = address.getHouseAddress();
+    public void houseNumberCase(){
+        address.setHouseNumber("1103/52");
+        result = address.getHouseNumber();
         assert (result.contains("1103/52"));
-        
+    }
+    
+    @Test
+    public void villageCase(){
         address.setVillage("Jarurat");
         result = address.getVillage();
         assert (result.contains("Jarurat"));
-        
+    }
+    
+    @Test
+    public void citySubDivisionSectionCase(){
         address.setCitySubDivisionSection(1);
         resultForCheckCitySubDivisionSection = address.getCitySubDivisionSection();
         assert (resultForCheckCitySubDivisionSection==1);
-        
-        address.setStreet("Petchburi");
+    }
+    
+    @Test
+    public void streetCase(){
+        address.setStreet("Petchaburi");
         result = address.getStreet();
         assert (result.contains("Petchaburi"));
-        
-        address.setAlley("Petchburi31");
+    }
+    
+    @Test
+    public void alleyCase(){
+        address.setAlley("Petchaburi31");
         result = address.getAlley();
         assert (result.contains("Petchaburi31"));
-        
+    }
+    
+    @Test
+    public void citySubDivisionCase(){
         address.setCitySubDivision("Makkasan");
         result = address.getCitySubDivision();
         assert (result.contains("Makkasan"));
-        
+    }
+    
+    @Test
+    public void cityCase(){
         address.setCity("Ratchatewee");
         result = address.getCity();
         assert (result.contains("Ratchatewee"));
-        
+    }
+    
+    @Test
+    public void countrySubDivisionCase(){
         address.setCountrySubDivision("Bangkok");
         result = address.getCountrySubDivision();
         assert (result.contains("Bangkok"));
-        
+    }
+    
+    @Test
+    public void countryCase(){
         address.setCountry("Thailand");
         result = address.getCountry();
         assert (result.contains("Thailand"));
+    }
+    
+    @Test
+    public void getAllDataFromInstanceCase(){
+        address.setHouseNumber("1103/52");
+        address.setVillage("Jarurat");
+        address.setCitySubDivisionSection(1);
+        address.setStreet("Petchaburi");
+        address.setAlley("Petchaburi31");
+        address.setCitySubDivision("Makkasan");
+        address.setCity("Ratchatewee");
+        address.setCountrySubDivision("Bangkok");
         
         resultArray = address.getAddress();
         assert (resultArray[0].contains("1103/52"));
@@ -58,5 +93,4 @@ public class AddressTest {
         assert (resultArray[7].contains("Bangkok"));
         assert (resultArray[8].contains("Thailand"));
     }
-    
 }
