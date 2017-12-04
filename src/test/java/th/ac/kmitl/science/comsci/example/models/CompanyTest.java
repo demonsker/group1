1950903248ecf7ad298f123b5274bdfda0c3c1f3
Nattadet <tooi_321@hotmail.com>
2017-12-04 18:07:36 +0700

    Add test case of address on CompanyTest

--- src/test/java/th/ac/kmitl/science/comsci/example/models/CompanyTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/CompanyTest.java
@@ -46,6 +46,38 @@ public class CompanyTest {
         seller.setUniversalCommunication(newUriId);
         assert (seller.getUniversalCommunication().getUriId().equals(newUriId));
    }
+   
+   @Test
+    public void getAllDataFromAddressInstanceCase(){
+        String id = "id57050269";
+        String name = "Patipon Taweechat";
+        String globalId = "GId57050269";
+        String taxId = "TId57050269";
+        String []resultArray = new String[9];
+
+        Company seller = new Company(id, name, globalId, taxId);
+        
+        Address address = seller.getAddressInstance();
+        address.setHouseNumber("1103/52");
+        address.setVillage("Jarurat");
+        address.setCitySubDivisionSection(1);
+        address.setStreet("Petchaburi");
+        address.setAlley("Petchaburi31");
+        address.setCitySubDivision("Makkasan");
+        address.setCity("Ratchatewee");
+        address.setCountrySubDivision("Bangkok");
+        
+        resultArray = address.getAddress();
+        assert (resultArray[0].contains("1103/52"));
+        assert (resultArray[1].contains("Jarurat"));
+        assert (resultArray[2].contains("1"));
+        assert (resultArray[3].contains("Petchaburi"));
+        assert (resultArray[4].contains("Petchaburi31"));
+        assert (resultArray[5].contains("Makkasan"));
+        assert (resultArray[6].contains("Ratchatewee"));
+        assert (resultArray[7].contains("Bangkok"));
+        assert (resultArray[8].contains("Thailand"));
+    }
 
 }
 
