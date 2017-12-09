f3a5775446d30fc399863a1fcd03b140324cd1ea
poyapoyassgit <33254732+poyapoyassgit@users.noreply.github.com>
2017-12-09 17:56:59 +0700

    Change CitySubDivisionCode

--- src/test/java/th/ac/kmitl/science/comsci/example/models/CitySubDivisioMappingTest.java
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/CitySubDivisioMappingTest.java
@@ -5,24 +5,24 @@ import th.ac.kmitl.science.comsci.example.models.mocks.AddressMock;
 
 public class CitySubDivisioMappingTest {
     
-    private CitySubDivisionName citySubDivisionMapping = new CitySubDivisionName();
+    private CitySubDivisionMapping citySubDivisionMapping = new CitySubDivisionMapping();
     private AddressMock mock = new AddressMock();
     
     @Test
     public void testByName(){
-        assert(citySubDivisionMapping.mapper("Si Lom") == 2);
-        assert(citySubDivisionMapping.mapper("Bukkhalo") == 4);
-        assert(citySubDivisionMapping.mapper("Din Daeng") == 1);
-        assert(citySubDivisionMapping.mapper("Don Mueang") == 2);
-        assert(citySubDivisionMapping.mapper("Khan Na Yao") == 1);
-        assert(citySubDivisionMapping.mapper("Thap Yao") == 5);
-        assert(citySubDivisionMapping.mapper("Lumphini") == 4);
-        assert(citySubDivisionMapping.mapper("Lam Pla Thio")== 4);
+        assert(citySubDivisionMapping.mapper("Si Lom") == 100402);
+        assert(citySubDivisionMapping.mapper("Bukkhalo") == 101504);
+        assert(citySubDivisionMapping.mapper("Din Daeng") == 102601);
+        assert(citySubDivisionMapping.mapper("Don Mueang") == 103604);
+        assert(citySubDivisionMapping.mapper("Khan Na Yao") == 104301);
+        assert(citySubDivisionMapping.mapper("Thap Yao") == 101105);
+        assert(citySubDivisionMapping.mapper("Lumphini") == 100704);
+        assert(citySubDivisionMapping.mapper("Lam Pla Thio")== 101104);
     }
     
     @Test
     public void testByMock(){
-        assert(citySubDivisionMapping.mapper(mock.citySubDivision) == 4);
+        assert(citySubDivisionMapping.mapper(mock.citySubDivision) == 103704);
     }
     
 }
